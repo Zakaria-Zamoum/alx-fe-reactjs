859a577e1da04208be9a8eaa6aee916d2104c9a9
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-11-08 09:54:23 +0100

    update

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,14 +1,13 @@
 import { useContext } from 'react';
-import UserContext from '../UserContext';
+import { UserContext } from './UserContext';
 
 function UserProfile() {
-  const user = useContext(UserContext);
+  const userData = useContext(UserContext);
 
   return (
     <div>
-      <h2>User Profile</h2>
-      <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
     </div>
   );
 }
