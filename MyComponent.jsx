55e0fa83f6579ec4d194f119e7ff95c12667cf7e
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-11-08 07:54:35 +0100

    Add alx-react-app-new with inline CSS styling

---
+++ MyComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyComponent() {
+  return (
+    <div>
+      <h1>Hello from MyComponent</h1>
+    </div>
+  );
+}
+
+export default MyComponent;
+
+<p>This is extra content</p>
