31c51e50e77446a742cd013033f7452e7f74c2d9
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-11-08 09:06:55 +0100

    UPDATE

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,7 +2,7 @@ import UserContext from './UserContext';
 import UserProfile from './components/UserProfile';
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Zakaria Zamoum", email: "zakaria@example.com" };
 
   return (
     <UserContext.Provider value={userData}>
