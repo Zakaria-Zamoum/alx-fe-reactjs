963cdbbdd1392fe3e93ea42900bb65f16413a17f
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-11-08 13:24:05 +0100

    Fix my-company folder as normal directory

---
+++ my-company/alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: '#ccc', textAlign: 'center', padding: '15px' }}>
+      <p>© 2025 My Favorite Cities</p>
+    </footer>
+  );
+}
+
+export default Footer;
