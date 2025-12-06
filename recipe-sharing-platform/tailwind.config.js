9a47fbf17677e662306519ac424fd08f68c7879c
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-12-06 15:47:03 +0100

    update

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    "./index.html",           // root HTML file
+    "./public/index.html",    // public HTML file
+    "./src/**/*.{js,jsx,ts,tsx}" // all React source files
+  ],
   theme: {
     extend: {},
   },
