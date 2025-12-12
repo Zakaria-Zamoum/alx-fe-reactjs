df1952e14af43bad86b2c7978fe3bf13e2ed58fb
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-12-12 12:00:51 +0100

    update

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
+import FormikForm from "./components/formikForm";
 
 function App() {
   return (
     <div>
       <RegistrationForm />
       <hr />
-      <FormikForm />
+      <formikForm />
     </div>
   );
 }
