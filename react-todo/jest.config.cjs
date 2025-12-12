14e4ef3a83a3c2fa55a6eb1745388775643c9ae5
Zakaria Zamoum <zakariazamoum0@gmail.com>
2025-12-12 12:56:42 +0100

    update

--- react-todo/jest.config.cjs
+++ react-todo/jest.config.cjs
@@ -1,8 +1,5 @@
+// jest.config.cjs
 module.exports = {
-  testEnvironment: 'jsdom',
-  testMatch: ['**/__tests__/**/*.test.js'],
-  transform: {
-    '^.+\\.(js|jsx)$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.cjs'],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["@testing-library/jest-dom"],
 };
